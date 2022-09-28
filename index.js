29e66b0d56b57b10503bf752f473fe949bed22b1
Racknaro <racknarow1@gmail.com>
2022-09-28 02:39:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const productos = require('./router/productosRouter');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT;
+const ENV = process.env.NODE_ENV;
+
+//! view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/* app.use('/', express.static(path.join(__dirname, 'public'))) //! aca se declaran los archivos estaticos */
+
+app.use('/', productos);
+
+//! Middleware de manejo de errores
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `Servidor http esta escuchando en el puerto ${server.address().port}`
+    );
+    console.log(`http://localhost:${server.address().port}`);
+    console.log(`Environment:${ENV}`);
+});
+
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
